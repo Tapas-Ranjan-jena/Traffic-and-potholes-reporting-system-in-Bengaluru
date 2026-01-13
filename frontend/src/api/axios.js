36cfa91c0cf262a776b58ad623d79c61cf1b60f8
Tapas Ranjan Jena <tapasranjan1174@gmail.com>
2026-01-13 13:08:29 +0530

    update API

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+ baseURL: "https://traffic-and-potholes-reporting-system-in.onrender.com/api"
+
+
 });
 
 // Attach token automatically
